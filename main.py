2317876f9fe8911b8470d1f82564fa2f6cabbcb3
Melanie Jaimes <113930529+majaimes1@users.noreply.github.com>
2023-02-16 18:41:00 +0000

    learned how read files, write and append

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 # remember to fork this and push to github
 
+
+# f = open('employees.txt', 'reading and writing files in python.pdf')
+# f.write(" i love watching tik tok")
+# read give us everything
+# readline gives us the first line
+# print(f.readline())
+
+# f.close()
+# with open("test.txt", 'r') as f:
+#   # f_contents = f.read()
+#   f.write("krgofilje[r8qgblih")
+#   print(f)
+# with open("test.txt", 'w') as f:
+
+#     for line in f:
+#       print(line, end = '')
+with open('period7.txt', 'a') as f:
+  f.write(" lol ")
+  
